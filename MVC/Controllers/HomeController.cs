b2874e9a98f2f20805bdeeeaac726b3d5d59fb31
Chris Saless <cmsaless@buffalo.edu>
2019-07-05 22:45:54 -0400

    worked on profile page

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace MVC.Controllers
             return View();
         }
 
+        public IActionResult Out()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";

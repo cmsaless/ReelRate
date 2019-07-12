d803e937359f424936cbe28cdcc099153475d2fb
Chris Saless <cmsaless@buffalo.edu>
2019-07-12 08:21:58 -0400

    updated startup.cs

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace MVC
 
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("Server = tcp:reelrate.database.windows.net, 1433; Initial Catalog = ReelRate; Persist Security Info = False; User ID = cmsaless; Password = Qw3rtyuiop[; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;")));
+                options.UseSqlServer(Configuration.GetConnectionString("Server=tcp:reelrate.database.windows.net,1433;Initial Catalog=ReelRate;Persist Security Info=False;User ID=cmsaless;Password=Qw3rtyuiop[;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")));
 
             services.AddScoped<IRepository<Movie>, SQLRepository<Movie>>();
             services.AddScoped<IRepository<MovieList>, SQLRepository<MovieList>>();
